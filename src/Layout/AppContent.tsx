33506a25b59ef351bca5364c6855716a5e7f5e7e
Ben ROUVIERE <rouviere.benjamin@gmail.com>
2020-09-11 20:51:00 +0200

    Add profiles route

--- src/Layout/AppContent.tsx
+++ src/Layout/AppContent.tsx
@@ -2,12 +2,14 @@ import * as React from 'react';
 import { Switch, Route } from 'react-router-dom';
 // import UsersList from '../Users/Components/UsersList';
 import LoginPage from '../Login/components/LoginPage';
+import UsersList from '../Users/Components/UsersList';
 
 class AppContent extends React.Component {
 
   render(){
     return (
       <Switch>
+        <Route path="/profiles"><UsersList /></Route>
         <Route path="/login"><LoginPage></LoginPage></Route>
         <Route path="/" component={LoginPage} />
       </Switch>
--- src/Login/components/LoginForm.tsx
+++ src/Login/components/LoginForm.tsx
@@ -1,9 +1,11 @@
 import * as React from 'react';
 import { IFormField } from '../../Utils/Types';
-import { TextField, Button } from '@material-ui/core';
+import { TextField, Button, Container, Box, Grid } from '@material-ui/core';
 import { login } from '../../Api/UserApi';
 import history from '../../history';
 import { validateRequiredField } from '../../Utils/ValidateRequiredField';
+import { Alert } from '@material-ui/lab';
+import DoneOutlineIcon from '@material-ui/icons/DoneOutline';
 
 interface LoginFormState {
   email: IFormField;
@@ -15,8 +17,8 @@ class LoginForm extends React.Component<{}, LoginFormState> {
   constructor(props: {}){
     super(props)
     this.state= {
-      email: {value: '', isValid: true},
-      password: {value: '', isValid: true},
+      email: {value: '', isValid: false},
+      password: {value: '', isValid: false},
       status: 'ready'
     }
   }
@@ -28,40 +30,61 @@ class LoginForm extends React.Component<{}, LoginFormState> {
   }
 
   render() {
+    const { email, password, status } = this.state;
     return (
-      // {this.state.status !== 'ready' ? <Alert severity={this.state.status}></Alert> : null}
-      <div>
-      <h2>Log In</h2>
-      <form onSubmit={(event) => {
-        event.preventDefault();
-        this._submit();
-      }}>
-        <TextField 
-          label="Email"
-          required={true}
-          value={this.state.email.value}
-          variant="outlined"
-          {...( this.state.email.isValid ? {} : {error: true, helperText: 'Ce champ est obligatoire'})}
-          onChange={(event) => this.setState({
-            ...this.state,
-            email: {value: event.target.value, isValid: validateRequiredField(event.target.value)}})}
-        />
-        <TextField 
-          label="Password"
-          type="password"
-          required={true}
-          variant="outlined"
-          {...( this.state.password.isValid ? {} : {error: true, helperText: 'Ce champ est obligatoire'})}
-          value={this.state.password.value}
-          onChange={(event) => this.setState({
-            ...this.state,
-            password: {value: event.target.value, isValid: validateRequiredField(event.target.value)}})}
-        />
-        <Button variant="contained" type="submit">Submit</Button>
+    <Container maxWidth='xs'>
+      {status !== 'ready' ?
+        <Alert severity={status}>
+          {status === 'success' ? 'Utilisateur connecté' : 'Utilisateur inexistant'}
+        </Alert> : null }
+      <form onSubmit={(event) => { event.preventDefault(); this._submit() }}>
+        <Box style={{ margin: '2rem 0'}}>
+          <TextField
+            label="Email"
+            value={email.value}
+            required={true}
+            onChange={(event) => this.setState({
+              email: {value: event.target.value, isValid: validateRequiredField(event.target.value)}
+            })}
+            fullWidth={true}
+            style={{margin: '0.5rem 0'}}
+            variant="outlined"
+
+            {...( email.isValid && !!email.value ? {} : { error: true, helperText: "Ce champ est obligatoire" })}
+          />
+          <TextField
+            type="password"
+            label="Password"
+            required={true}
+            value={password.value}
+            onChange={(event) => this.setState({
+              password: {value: event.target.value, isValid: validateRequiredField(event.target.value)}
+            })}
+            fullWidth={true}
+            variant="outlined"
+
+            {...( password.isValid ? {} : { error: true, helperText: "Ce champ est obligatoire" })}
+          />
+        </Box>
+        <Box style={{margin: '1rem 0'}}>
+          <Grid container justify='flex-end'>
+            <Grid item xs={4}>
+              <Button
+                color="primary"
+                variant="contained"
+                type="submit"
+                disabled={!email.isValid || !password.isValid}
+                startIcon={<DoneOutlineIcon />}
+                fullWidth={true}
+              >
+                Submit
+              </Button>
+            </Grid>
+          </Grid>
+        </Box>
       </form>
-    </div>
-    )
-  }
+    </Container>
+  )}
 
 }
 
